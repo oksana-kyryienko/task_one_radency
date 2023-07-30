98ff86d699e269d0d8b4fb77898378937356c1e4
Oksana Kyryienko <kseniyals1609@gmail.com>
2023-07-31 00:04:59 +0300

    demo link was added

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "semi": ["error", "always"]
+  }
+};
